c1c20dadd8489f4642be3a98178f74d33141929b
aracool82 <aracool1982@gmail.com>
2024-11-23 18:53:54 +0700

    Version4_1

--- Scripts/Spawner.cs
+++ Scripts/Spawner.cs
@@ -6,19 +6,20 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private Observer _observer;
     [SerializeField] private Cube _cubePrefab;
-    
+
     [SerializeField] private int _initCubeCount;
     [SerializeField] private float _radius = 2f;
 
     private int divider = 2;
-
+    
     public event Action<Vector3, float> Exploding;
 
-    private void Awake()
+    private void Start()
     {
         _cubePrefab.transform.position = transform.position;
+        CreateCubes(_cubePrefab, _initCubeCount);
     }
-
+    
     private void OnEnable()
     {
         _observer.ClickedCube += OnClick;
@@ -29,40 +30,35 @@ public class Spawner : MonoBehaviour
         _observer.ClickedCube -= OnClick;
     }
 
-    private void Start()
-    {
-        int initChance = 100;
-
-        for (int i = 0; i < _initCubeCount; i++)
-            CreateNewCube(_cubePrefab, initChance, Vector3.one);
-    }
-
     private void OnClick(Cube cube)
     {
         if (cube.TrySplit())
-            CreateCubes(cube);
+            CreateCubes(cube, GetRandomCount(),true);
         else
             Exploding?.Invoke(cube.transform.position, cube.transform.localScale.y);
 
         Destroy(cube.gameObject);
     }
 
-    private void CreateCubes(Cube cube)
+    private void CreateCubes(Cube cube, int count,bool isChangingParamCube = false)
     {
-        int chance = cube.ChanceSplit / divider;
-        Vector3 scale = cube.transform.localScale / divider;
+        int chance = cube.ChanceSplit;
+        Vector3 scale = cube.transform.localScale;
+       
+        if (isChangingParamCube)
+        {
+            chance = cube.ChanceSplit / divider;
+            scale = cube.transform.localScale / divider;
+        }
 
-        for (int i = 0; i < GetRandomCount(); i++)
-            CreateNewCube(cube, chance, scale);
-    }
-
-    private void CreateNewCube(Cube cube, int chance, Vector3 scale)
-    {
-        Vector3 position = cube.transform.position + Random.onUnitSphere * _radius;
-        position.y = Mathf.Abs(position.y);
-
-        Cube newCube = Instantiate(cube, position, Quaternion.identity);
-        newCube.Initialize(chance, scale);
+        for (int i = 0; i < count; i++)
+        {
+            Vector3 position = cube.transform.position + Random.onUnitSphere * _radius;
+            position.y = Mathf.Abs(position.y);
+            
+            Cube newCube = Instantiate(cube, position, Quaternion.identity);
+            newCube.Initialize(chance, scale);
+        }
     }
 
     private int GetRandomCount()
